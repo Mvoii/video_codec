345c10016574358cd61b6955e0af98fa81631a08
Franklin Mvoi <mvoifranklin@gmail.com>
2025-01-23 00:33:28 +0300

    run script edit: added "|| exit" to the cd's incase of failure

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # start backend server
-cd backend
+cd backend || exit
 python -m flask run &
 P1=$!
 
 # start front end server
-cd ../frontend
+cd ../frontend || exit
 npm run dev &
 P2=$!
 
